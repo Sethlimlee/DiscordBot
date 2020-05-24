bc4ccd9291ef25e1d4046c229b04aa48e52d9186
Seth Lee <seth.lee@legrand.us>
2020-05-23 21:32:43 -0600

    updated with !cod command

--- bot.js
+++ bot.js
@@ -15,12 +15,13 @@ var bot = new Discord.Client({
   token: process.env.token,
   autorun: true
 });
-bot.on("ready", function(evt) {
+bot.on("ready", function (evt) {
   logger.info("Connected");
   logger.info("Logged in as: ");
   logger.info(bot.username + " - (" + bot.id + ")");
 });
-bot.on("message", function(user, userID, channelID, message, evt) {
+
+bot.on("message", function (user, userID, channelID, message, evt) {
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
   if (message.substring(0, 1) == "!") {
@@ -34,9 +35,10 @@ bot.on("message", function(user, userID, channelID, message, evt) {
       case "apex":
         axios
           .get(
-            "https://public-api.tracker.gg/apex/v1/standard/profile/5/" + name,
-            {
-              headers: { "TRN-Api-Key": process.env.apiKey }
+            "https://public-api.tracker.gg/apex/v1/standard/profile/5/" + name, {
+              headers: {
+                "TRN-Api-Key": process.env.apiKey
+              }
             }
           )
           .then(response => {
@@ -65,7 +67,7 @@ bot.on("message", function(user, userID, channelID, message, evt) {
                 discordMessage = results.join(" " + os.EOL);
                 bot.sendMessage({
                   to: channelID,
-                  message: discordMessage + os.EOL + os.EOL + "**Current Sopa Bot Commands: !Apex + Origin Username, !Natalie, !Eddie**"
+                  message: discordMessage + os.EOL + os.EOL + "**Current Sopa Bot Commands: !Cod + Battle.Net Name (SopaGrande#1490), !Apex + Origin Username, !Natalie, !Eddie**"
                 });
               }
             } else {
@@ -75,46 +77,114 @@ bot.on("message", function(user, userID, channelID, message, evt) {
               });
             }
           })
-          .catch(function(error) {
+          .catch(function (error) {
             bot.sendMessage({
               to: channelID,
-              message:
-                "uhh couldn't find " +
+              message: "uhh couldn't find " +
                 name +
                 " in the database, is that the right name?" +
-                os.EOL + os.EOL + "**Current Sopa Bot Commands: !Apex + Origin Username, !Natalie, !Eddie**"
+                os.EOL + os.EOL + "**Current Sopa Bot Commands: !Cod + Battle.Net Name (SopaGrande#1490), !Apex + Origin Username, !Natalie, !Eddie**"
             });
           });
         break;
-      // !natalie
+
+        // !natalie
       case "natalie":
         axios
           .get("https://api.thecatapi.com/v1/images/search", {
-            headers: { "x-api-key": process.env.catKey }
+            headers: {
+              "x-api-key": process.env.catKey
+            }
           })
           .then(response => {
             bot.sendMessage({
               to: channelID,
-              message:
-                response.data[0].url +
-                os.EOL + os.EOL + "**Current Sopa Bot Commands: !Apex + Origin Username, !Natalie, !Eddie**"
+              message: response.data[0].url +
+                os.EOL + os.EOL + "**Current Sopa Bot Commands: !Cod + Battle.Net Name (SopaGrande#1490), !Apex + Origin Username, !Natalie, !Eddie**"
             });
           });
-
         break;
+
+        // Eddie
       case "eddie":
         bot.sendMessage({
           to: channelID,
           message: "https://www.youtube.com/watch?v=c-ydGUHUDj8" +
-          os.EOL + os.EOL + "**Current Sopa Bot Commands: !Apex + Origin Username, !Natalie, !Eddie**"
+            os.EOL + os.EOL + "**Current Sopa Bot Commands: !Cod + Battle.Net Name (SopaGrande#1490), !Apex + Origin Username, !Natalie, !Eddie**"
         });
         break;
       case "help":
         bot.sendMessage({
           to: channelID,
-          message:
-            "**Current Sopa Bot Commands: !Apex + Origin Username, !Natalie, !Eddie**"
+          message: "**Current Sopa Bot Commands: !Cod + Battle.Net Name (SopaGrande#1490), !Apex + Origin Username, !Natalie, !Eddie**"
         });
+        break;
+
+        // !Cod
+      case "cod":
+        try {
+          var codName = name.split("#");
+        } catch {
+          bot.sendMessage({
+            to: channelID,
+            message: "Please enter a valid Name after !cod (EX: !cod Sopagrande#1490)" +
+              os.EOL + os.EOL + "**Current Sopa Bot Commands: !Cod + Battle.Net Name (SopaGrande#1490), !Apex + Origin Username, !Natalie, !Eddie**"
+          });
+          break;
+        }
+        axios
+          .get(
+            `https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/battle/gamer/${codName[0]}%23${codName[1]}/profile/type/wz`, {
+              headers: {
+                cookie: process.env.cookie
+              }
+            }
+          )
+          .then(response => {
+            if (response) {
+
+              var stats = response.data.data.weekly.mode.br_all.properties;
+
+              var matchesPlayed = `Matches Played: ${stats.matchesPlayed}`;
+              var kills = `Kills: ${stats.kills}`;
+              var deaths = `Deaths: ${stats.deaths}`;
+              var kD = `K/D: ${stats.kdRatio.toFixed(2)}`;
+              var gulagPercent = `Gulag Win Percentage: ${((stats.gulagKills / (stats.gulagKills + stats.gulagDeaths)) * 100).toFixed(2)}%`;
+              var averageDamageGivenPerGame = `Average Damage Done Per Game: ${(stats.damageDone / stats.matchesPlayed).toFixed(2)}`;
+              var averageDamageReceivedPerGame = `Average Damage Taken Per Game: ${(stats.damageTaken / stats.matchesPlayed).toFixed(2)}`;
+
+              var results = [];
+              results.push(`${codName[0]}'s Weekly Warzone Stats` + os.EOL)
+              results.push(matchesPlayed + os.EOL);
+              results.push(kills + os.EOL);
+              results.push(deaths + os.EOL);
+              results.push(kD + os.EOL);
+              results.push(gulagPercent + os.EOL);
+              results.push(averageDamageGivenPerGame + os.EOL);
+              results.push(averageDamageReceivedPerGame + os.EOL);
+
+              discordMessage = results.join(" " + os.EOL);
+
+              bot.sendMessage({
+                to: channelID,
+                message: discordMessage + os.EOL + os.EOL + "**Current Sopa Bot Commands: !Cod + Battle.Net Name (SopaGrande#1490), !Apex + Origin Username, !Natalie, !Eddie**"
+              });
+            } else {
+              bot.sendMessage({
+                to: channelID,
+                message: "Something went wrong, F in chat pls"
+              });
+            }
+          })
+          .catch(function (error) {
+            bot.sendMessage({
+              to: channelID,
+              message: "uhh couldn't find " +
+                name +
+                " in the database, is that the right name? Did you remember to add the # followed by your unique ID? (SopaGrande#1490)" +
+                os.EOL + os.EOL + "**Current Sopa Bot Commands: !Cod + Battle.Net Name (SopaGrande#1490), !Apex + Origin Username, !Natalie, !Eddie**"
+            });
+          });
         break;
     }
   }
